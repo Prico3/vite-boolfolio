cc9299dea54bff7f611cf57f10c96a15236f3ae7
Prico3 <pricoco.andrea@gmail.com>
2023-01-19 16:57:17 +0100

    push correzione

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,34 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from 'axios';
+export default {
+  name: 'App',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  created() {
+
+  },
+
+  methods: {
+    getPosts() {
+      axios.get('http://localhost:8000/api/posts').then(resp => {
+        console.log(resp);
+      })
+    }
+  }
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Pagina di Front Office</h1>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+@use './styles/general.scss' as *;
 </style>
